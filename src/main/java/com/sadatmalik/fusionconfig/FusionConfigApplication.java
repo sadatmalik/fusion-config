ba6b696b22c089a04f1af7ba03a4ab7ed88b1315
sadat.malik <sadat.malik@me.com>
2022-04-27 14:26:52 +0100

    Initial Spring.io files

---
+++ src/main/java/com/sadatmalik/fusionconfig/FusionConfigApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusionconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionConfigApplication.class, args);
+	}
+
+}
